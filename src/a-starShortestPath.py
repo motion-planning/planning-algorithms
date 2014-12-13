62e60928843afe30af9521e04030b13c8ea0f31a
SZanlongo <SZanlongo@users.noreply.github.com>
2014-12-12 19:52:21 -0500

    Added: a-star, a-star shortest path, RRT search

---
+++ src/a-starShortestPath.py
@@ -0,0 +1,30 @@
+# A* Search From http://www.redblobgames.com/pathfinding/a-star/implementation.html#sec-1-4
+
+def heuristic(a, b):
+   (x1, y1) = a
+   (x2, y2) = b
+   return abs(x1 - x2) + abs(y1 - y2)
+
+def a_star_search(graph, start, goal):
+   frontier = PriorityQueue()
+   frontier.put(start, 0)
+   came_from = {}
+   cost_so_far = {}
+   came_from[start] = None
+   cost_so_far[start] = 0
+   
+   while not frontier.empty():
+      current = frontier.get()
+      
+      if current == goal:
+         break
+      
+      for next in graph.neighbors(current):
+         new_cost = cost_so_far[current] + graph.cost(current, next)
+         if next not in cost_so_far or new_cost < cost_so_far[next]:
+            cost_so_far[next] = new_cost
+            priority = new_cost + heuristic(goal, next)
+            frontier.put(next, priority)
+            came_from[next] = current
+   
+   return came_from, cost_so_far
